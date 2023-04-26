ba713e678a5e02d0abb278e669f27aa7039222bc
MongsDev <43024098+MongsDev@users.noreply.github.com>
2023-04-26 14:03:21 +0900

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -3,19 +3,19 @@
 $active_group = 'maindb';
 $active_record = TRUE;
 
-$db['tranggle3']['hostname'] = 'dbhost:3306';
-$db['tranggle3']['username'] = 'username';
-$db['tranggle3']['password'] = 'password';
-$db['tranggle3']['database'] = 'database';
-$db['tranggle3']['dbdriver'] = 'mysqli';
-$db['tranggle3']['dbprefix'] = '';
-$db['tranggle3']['pconnect'] = TRUE;
-$db['tranggle3']['db_debug'] = (DEBUG === true); // (ENVIRONMENT !== 'production');
-$db['tranggle3']['cache_on'] = FALSE;
-$db['tranggle3']['cachedir'] = FCPATH . 'data/cache/db';
-$db['tranggle3']['char_set'] = 'utf8';
-$db['tranggle3']['dbcollat'] = 'utf8_general_ci';
-$db['tranggle3']['swap_pre'] = '';
-$db['tranggle3']['autoinit'] = TRUE;
-$db['tranggle3']['stricton'] = FALSE;
-$db['tranggle3']['save_queries'] = TRUE; // Query Loging
+$db['master']['hostname'] = 'dbhost:3306';
+$db['master']['username'] = 'username';
+$db['master']['password'] = 'password';
+$db['master']['database'] = 'database';
+$db['master']['dbdriver'] = 'mysqli';
+$db['master']['dbprefix'] = '';
+$db['master']['pconnect'] = TRUE;
+$db['master']['db_debug'] = (DEBUG === true); // (ENVIRONMENT !== 'production');
+$db['master']['cache_on'] = FALSE;
+$db['master']['cachedir'] = FCPATH . 'data/cache/db';
+$db['master']['char_set'] = 'utf8';
+$db['master']['dbcollat'] = 'utf8_general_ci';
+$db['master']['swap_pre'] = '';
+$db['master']['autoinit'] = TRUE;
+$db['master']['stricton'] = FALSE;
+$db['master']['save_queries'] = TRUE; // Query Loging
